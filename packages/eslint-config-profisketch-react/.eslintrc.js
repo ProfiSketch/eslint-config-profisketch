37cb545fb3caa95946f39849e7a99b784aa625dd
Yakovlev Kll <yakovlev.kll@gmail.com>
2023-02-28 09:32:00 +0300

    feat: :sparkles: ESlint React config added

---
+++ packages/eslint-config-profisketch-react/.eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'next/core-web-vitals',
+    'plugin:tailwindcss/recommended',
+    'profisketch',
+  ],
+}
